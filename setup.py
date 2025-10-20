521e56d46717cb9620cdf51b8f5b436cc3efa02e
Wyatt Sellers <124455397+SurvivalW@users.noreply.github.com>
2025-10-20 19:33:38 -0400

    Done with basic TUI layout
    - updated setup.py
    - added setup system msg's

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mobop2p",
+    name="MoboP2P",
     version="0.0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    install_requires=[
+        "textual"
+    ],
     entry_points={
         "console_scripts": [
             "mobop2p = main:main",
--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
+#TEXTUAL
 from textual.app import App
 from textual.scroll_view import ScrollView
 from textual.widgets import Input
 from textual.widgets import Button
+from textual.widgets import RichLog
 from textual.containers import Vertical, Horizontal, Grid
+#DATETIME
+from datetime import datetime
+#RICH
+from rich.text import Text
+#ASYNCIO
+import asyncio
 
+
+#===========MAIN===========
 class MoboP2P(App):
 
     CSS_PATH = "messaging/style.css"
@@ -12,20 +22,31 @@ class MoboP2P(App):
         with Vertical():
             with Horizontal():#top
                 with Vertical(id="chat_container"):#chat
-                    yield ScrollView(id="chatLog")
+                    yield RichLog(id="chatLog")
                     yield Input(id="input")
                 with Vertical(id="contacts_container"):#contacts view and buttons
                     yield ScrollView(id="contacts")
                     with Horizontal(id="buttons_container"):#button container
                         yield Button(id="add")
                         yield Button(id="remove")
-            yield ScrollView(id="systemMessages")#bottom
+            yield RichLog(id="systemMessages")#bottom
 
 
     def on_mount(self):
-        pass
+        self.sysms_view = self.query_one("#systemMessages", RichLog)
+        self.sysms_view.clear()
+        self.send_SYS_message("[bold red]MoboP2P started...[/bold red]")#start up msg
+
+        self.chat_view = self.query_one("#chatLog", RichLog)
+        self.chat_view.clear()
 
 
+    def send_SYS_message(self, msg: str):
+        timestamp = datetime.now().strftime("[%H:%M:%S]")
+        styled_text = Text.from_markup(f"[dim]{timestamp}[/dim] {msg}")
+        self.sysms_view.write(styled_text)
+
+#Booted up
 def main():
     MoboP2P().run()
 
